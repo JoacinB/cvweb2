f3a495d90f2a95b261d2dd2d68be0a267273aa4d
JoacinB <joaquinborgesborio@gmail.com>
2023-03-26 02:02:16 -0300

    Deco box pp

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ function Header() {
   const { colorMode } = useColorMode();
   return (
     <Box
-      className="h-full flex flex-col w-16 bg-black fixed top-0 right-0 border-solid border-violet-600 border-l-4"
+      className="h-full flex flex-col w-16 bg-black fixed top-0 right-0 border-solid border-violet-600 border-l-0"
       bg={colorMode === "dark" ? "black" : "white"}
     >
       <Flex className="flex-col h-full">
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,19 +4,23 @@ import { useColorMode } from "@chakra-ui/react";
 
 export default function Home() {
   const { colorMode } = useColorMode();
-  const shadowColor = { light: "shadow-[5px_8px_30px_-15px_rgba(0,0,0,1)]", dark: "shadow-[5px_8px_30px_-15px_rgba(255,255,255,1)]" };
+  const shadowColor = {
+    light: "shadow-[5px_8px_30px_-15px_rgba(0,0,0,1)]",
+    dark: "shadow-[5px_8px_30px_-15px_rgba(255,255,255,1)]",
+  };
   return (
     <Box>
       <Header />
-      {
-        <div className="flex justify-start w-full">
+      <Box className="flex justify-start items-center w-full h-screen">
+        <Box className="relative w-full">
           <Image
             src="profile-img.png"
             alt="Profile Image"
-            className={`rounded-[32px] ml-20 mt-40 mb-32 ${shadowColor[colorMode]}`}
+            className={`rounded-[32px] ${shadowColor[colorMode]} ml-24`}
           />
-        </div>
-      }
+        </Box>
+        <Box className={`absolute top-0 h-full flex flex-col -z-[1] w-80 ${colorMode === "dark" ? "bg-gray-800" : "bg-gray-300"}`} />
+      </Box>
     </Box>
   );
 }
